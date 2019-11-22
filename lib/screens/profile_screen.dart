83890bbf8f3a9634003f9a2031c621ba0c60797b
Shaniel Samadhan <shanielsamadhan@Shaniels-iMac.local>
2019-11-21 21:29:35 -0300

    Working on profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -8,25 +6,14 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = GoogleSignIn();
-
-  Future<String> signOut() async {
-    await googleSignIn.signOut();
-    await _auth.signOut();
-    print(' sign out');
-    return 'error';
-  }
-
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text('logout'),
-      onPressed: () {
-        signOut().whenComplete(() {
-          Navigator.pushReplacementNamed(context, '/login');
-        });
-      },
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Text('profile'),
+        ],
+      ),
     );
   }
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SettingsScreen extends StatefulWidget {
   @override
@@ -6,8 +8,25 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<String> signOut() async {
+    await googleSignIn.signOut();
+    await _auth.signOut();
+    print(' sign out');
+    return 'error';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return RaisedButton(
+      child: Text('logout'),
+      onPressed: () {
+        signOut().whenComplete(() {
+          Navigator.pushReplacementNamed(context, '/login');
+        });
+      },
+    );
   }
 }
